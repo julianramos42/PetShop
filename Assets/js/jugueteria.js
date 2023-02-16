85c39120c94f7bb6ffc401b909478d35f0a984dc
Julian <julianete4@gmail.com>
2023-02-16 16:57:46 -0300

    juguetes y farmacos dinamicos con un modal

--- Assets/js/jugueteria.js
+++ Assets/js/jugueteria.js
@@ -1,28 +1,79 @@
-import { getData, createCards, filterProducts,writeSponsors,createCarru } from "./module/functions.js";
+import { getData, createCards, filterProducts, writeSponsors, createCarru, replaceModal } from "./module/functions.js";
 
 const container = document.getElementById("card-container")
 const searchBar = document.getElementById("search-bar")
 
-let data = getData()
-data.then( (response) => {
-    let toys = response.filter( (product) => product.categoria === "jugueteria")
-    createCards(toys,container,"")
+let products = JSON.parse(localStorage.getItem("products")) || [] // trae del local storage los productos que fueron agregados al carrito
 
-    searchBar.addEventListener( "keyup", (e) => {
+let data = getData()
+data.then((response) => {
+    if (!JSON.parse(localStorage.getItem("toys"))) {
+        let items = response.filter((product) => product.categoria === "jugueteria") // guarda en items los productos con categoria "jugueteria" traidos del fetch
+        localStorage.setItem("toys", JSON.stringify(items)) // crea una propiedad en localStorage donde la key es "toys" y el value son todos los items
+    }
+
+    let toys = JSON.parse(localStorage.getItem("toys")) || [] // toma el value de la key "toys" en el localStorage y lo guarda en la variable toys
+    createCards(toys, container, "")
+
+    searchBar.addEventListener("keyup", (e) => {
         let filteredToys = filterProducts(toys, e.target.value.toLowerCase())
-        createCards(filteredToys,container,e.target.value.toLowerCase())
+        createCards(filteredToys, container, e.target.value.toLowerCase())
     })
+
+    container.addEventListener("click", (e) => {
+        if (e.target.localName === "button") {
+            let pressToy = toys.find(toy => toy._id == e.target.id)
+            if (pressToy.disponibles > 0) {
+                products.push(pressToy)
+                localStorage.setItem("products", JSON.stringify(products))
+                for (let toy of toys) {
+                    if (toy == pressToy) {
+                        let i = toys.indexOf(toy)
+                        pressToy.disponibles--
+                        toys[i] = pressToy
+                        localStorage.setItem("toys", JSON.stringify(toys))
+
+                        let modalContainer = document.getElementById("card-container")
+                        let modals = Array.from(document.getElementById("card-container").children)
+                        let modalToChange = modals.filter( (child) => child.className.includes("show") )
+                        let newModal = replaceModal(pressToy)
+                        let modalIndex = modals.indexOf(modalToChange[0])
+                        console.log(modals[modalIndex])
+                        modals[modalIndex].innerHTML = newModal
+                        console.log(modals[modalIndex])
+                    }
+                }
+            }
+        }
+        else if (e.target.offsetParent && e.target.offsetParent.className == "card producto") { //si existe un parent y ese parent tiene className card y producto
+            let modal = e.target.offsetParent.nextElementSibling // el elemento que le sigue al parent, en este caso el modal
+            modal.addEventListener("click", (e) => {
+                if (e.target.className.includes("modal-container")) {
+                    createCards(toys, container, "") //cuando se clickee afuera del modal se actualizan las cards
+                }
+            })
+        }
+    })
+
 })
 
 
 let slideTrack = document.getElementById("slide-track")
 
-let array = ["dog-chow.png","dog-selection.png","dogui.png","kongo.jpg","pedigree.png","proplan.png","royal-canin.png","sabrocitos.png","whiscas.png","dog-chow.png","dog-selection.png","dogui.png","kongo.jpg","pedigree.png","proplan.png","royal-canin.png","sabrocitos.png","whiscas.png"]
+let array = ["dog-chow.png", "dog-selection.png", "dogui.png", "kongo.jpg", "pedigree.png", "proplan.png", "royal-canin.png", "sabrocitos.png", "whiscas.png", "dog-chow.png", "dog-selection.png", "dogui.png", "kongo.jpg", "pedigree.png", "proplan.png", "royal-canin.png", "sabrocitos.png", "whiscas.png"]
 
 writeSponsors(array, slideTrack)
 
 let slide = document.getElementById("slide")
 
-let array2 = ["dog.jpg","pexels-adam-kontor-333083.jpg","pexels-kat-smith-551628.jpg","dog-ball.jpg","carpincho.jpg"]
+let array2 = ["dog.jpg", "pexels-adam-kontor-333083.jpg", "pexels-kat-smith-551628.jpg", "dog-ball.jpg", "carpincho.jpg"]
 
-createCarru(array2,slide)
+createCarru(array2, slide)
+
+
+// PARA BORRAR ITEMS DEL CARRITO
+
+// if( products.some( product => product._id == e.target.id) ){
+//     products = products.filter( (product) => product._id != e.target.id)
+//     localStorage.setItem("products", JSON.stringify(products))
+// }
--- Assets/js/module/functions.js
+++ Assets/js/module/functions.js
@@ -1,68 +1,131 @@
 export async function getData() {
   try {
-      const response = await fetch("https://mindhub-xj03.onrender.com/api/petshop")
-      const data = await response.json()
-      return data
+    const response = await fetch("https://mindhub-xj03.onrender.com/api/petshop")
+    const data = await response.json()
+    return data
   }
   catch (error) {
-      console.log(`Error: ${error}`)
+    console.log(`Error: ${error}`)
   }
 }
 
-export function createCards(list, container,formulario) {
+export function createCards(list, container, formulario) {
   container.innerHTML = ""
   let aux = ""
-  if(list.length === 0){
-    noEncontrado(container,formulario)
-  }else{
+  if (list.length === 0) {
+    noEncontrado(container, formulario)
+  } else {
 
     for (let element of list) {
-        aux += writeCard(element)
+      aux += writeCard(element)
+      
     }
     container.innerHTML += aux
   }
 }
 
-export function writeCard(element) {
+function writeCard(element) {
   let color = element.disponibles < 5 ? "red" : "green";
   return `
-    <div class="card" style="width: 18rem;">
-        <div>
-            <img src="${element.imagen}"class="card-img-top tam_img_card" alt="${element.producto}">
-            <button class="favorite-btn"><img src="../img/heart.png" class="img-heart" alt="heart"></button>
+    <div class="card producto" id="card" style="width: 18rem;" data-bs-toggle="modal" data-bs-target="#${element._id}">
+      <div>
+        <img src="${element.imagen}"class="card-img-top tam_img_card" alt="${element.producto}">
+        <button class="favorite-btn"><img src="../img/heart.png" class="img-heart" alt="heart"></button>
+      </div>
+      <div class="card-body ">
+        <div class="cont-stock d-flex flex-column">
+          <p class="card-text"><b>Precio: $${element.precio}</b></p>
+          <p class=" ${color} text-center stock"><b>Stock</b></p>
+          <h5 >${element.producto}</h5>
         </div>
-        <div class="card-body ">
+        <div class="cont-unidad d-flex justify-content-end align-items-end">
+          <p>${element.disponibles} Unidades</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="${element._id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content w-100 mx-auto" id="modal-container-${element._id}">
+          <div class="modal-body d-flex justify-content-center gap-3">
+            <div class="card" style="width: 25rem;">
+              <div>
+                <img src="${element.imagen}"class="card-img-top tam_img_card" alt="${element.producto}">
+                <button class="favorite-btn"><img src="../img/heart.png" class="img-heart" alt="heart"></button>
+              </div>
+              <div class="card-body">
+                <div class="cont-stock d-flex flex-column">
+                  <p class="card-text"><b>Precio: $${element.precio}</b></p>
+                  <p class=" ${color} text-center stock"><b>Stock</b></p>
+                  <h5 >${element.producto}</h5>
+                </div>
+                <div class="cont-unidad d-flex justify-content-end align-items-end">
+                  <p>${element.disponibles} Unidades</p>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex flex-column justify-content-center" style="width: 25rem;">
+              <p>${element.descripcion}</p>
+              <button type="button" class="btn btn-primary" id="${element._id}">Agregar Al Carrito</button>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+export function replaceModal(element) {
+  let color = element.disponibles < 5 ? "red" : "green";
+  return `
+  <div class="modal fade show" id="${element._id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-modal="true" role="dialog" style="display: block;">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content w-100 mx-auto" id="modal-container-${element._id}">
+      <div class="modal-body d-flex justify-content-center gap-3">
+        <div class="card" style="width: 25rem;">
+          <div>
+            <img src="${element.imagen}" class="card-img-top tam_img_card" alt="${element.producto}">
+            <button class="favorite-btn"><img src="../img/heart.png" class="img-heart" alt="heart"></button>
+          </div>
+          <div class="card-body">
             <div class="cont-stock d-flex flex-column">
-                <p class="card-text"><b>Price: $${element.precio}</b></p>
-                <p class=" ${color} text-center stock"><b>Stock</b></p>
-                <h5 >${element.producto}</h5>
+              <p class="card-text"><b>Precio: $${element.precio}</b></p>
+              <p class="${color} text-center stock"><b>Stock</b></p>
+              <h5>${element.producto}</h5>
             </div>
             <div class="cont-unidad d-flex justify-content-end align-items-end">
-                <p>${element.disponibles} Unidades</p>
+              <p>${element.disponibles} Unidades</p>
             </div>
+          </div>
         </div>
+        <div class="d-flex flex-column justify-content-center" style="width: 25rem;">
+          <p>${element.descripcion}</p>
+          <button type="button" class="btn btn-primary" id="${element._id}">Agregar Al Carrito</button>
+        </div>      
+      </div>
     </div>
-    `
+  </div>
+</div>
+  `
 }
 
-
-export function filterProducts(products, value){
-  return products.filter( (product) => product.producto.toLowerCase().includes(value))
+export function filterProducts(products, value) {
+  return products.filter((product) => product.producto.toLowerCase().includes(value))
 }
 
-export function writeSponsors(list, container){
-    for(let i=0; i<18; i++){
-      container.innerHTML+=`
+export function writeSponsors(list, container) {
+  for (let i = 0; i < 18; i++) {
+    container.innerHTML += `
         <div class="cont-slide">
           <img src="../img/${list[i]}" alt="">
         </div>
       `
-    }
+  }
 }
 
-export function writeSponsorsHome(list, container){
-  for(let i=0; i<18; i++){
-    container.innerHTML+=`
+export function writeSponsorsHome(list, container) {
+  for (let i = 0; i < 18; i++) {
+    container.innerHTML += `
       <div class="cont-slide">
         <img src="./Assets/img/${list[i]}" alt="">
       </div>
@@ -70,9 +133,9 @@ export function writeSponsorsHome(list, container){
   }
 }
 
-export function createCarru(list, container){
-  
-    container.innerHTML+=`
+export function createCarru(list, container) {
+
+  container.innerHTML += `
       <div class="carousel-inner">
                       <div class="carousel-item active">
                         <img src="../img/${list[0]}" class="d-block w-100 border1" alt="${list[0]}">
@@ -88,11 +151,11 @@ export function createCarru(list, container){
                       </div>
                     </div>
     `
-  
+
 }
-export function createCarruHome(list, container){
-  
-  container.innerHTML+=`
+export function createCarruHome(list, container) {
+
+  container.innerHTML += `
     <div class="carousel-inner">
                       <div class="carousel-item active">
                         <img src="./Assets/img/${list[0]}" class="d-block w-100 border1" alt="${list[0]}">
@@ -110,12 +173,11 @@ export function createCarruHome(list, container){
   `
 }
 
-export function noEncontrado(container,formulario){
+export function noEncontrado(container, formulario) {
   container.innerHTML = `<div class="style-mens">
   <h5>No ha sido posible encontrar nada para "${formulario}".</h5>
   <h5>Prueba a detallar tu búsqueda.</h5>
   <img src="../img/cinnamon.png" alt="perro">
   </div>`
-  
-}
 
+}
