5710d0785bd0231c201927ec6ca6c267ccadf632
Julian <julianete4@gmail.com>
2023-02-17 18:33:05 -0300

    cambios al header

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,4 +1,16 @@
-import { getData, createCarruHome,writeSponsorsHome} from './module/functions.js'
+import { getData, createCarruHome,writeSponsorsHome, createShopping} from './module/functions.js'
+
+const carrito = document.getElementById("btn-car")
+const shopping = document.getElementById("cart")
+const modalCarrito = document.getElementById("modal-content")
+
+let products = JSON.parse(localStorage.getItem("products")) || [] // trae del local storage los productos que fueron agregados al carrito
+let precioTotal = 0
+createShopping(products,shopping,false)
+
+let toys = JSON.parse(localStorage.getItem("toys")) || [] // toma el value de la key "toys" en el localStorage y lo guarda en la variable toys
+let pharmacyProducts = JSON.parse(localStorage.getItem("pharmacyProducts")) || []
+let cartProducts = toys.concat(pharmacyProducts)
 
 let data = getData()
 data.then((response) => {
@@ -9,6 +21,66 @@ data.then((response) => {
         localStorage.setItem("pharmacyProducts", JSON.stringify(pharmacyProducts))
     }
 })
+
+carrito.addEventListener("click", (e) => {
+    precioTotal = 0
+    products.forEach(product => precioTotal += product.precio)
+    createShopping(products,shopping,precioTotal,false) // actualiza el modal del carrito
+
+    modalCarrito.addEventListener("click", (e) => {
+        if (e.target.className.includes("garbage")) {
+            let id = e.target.id
+            cartProducts.forEach(cartProduct => {
+                if (cartProduct._id == id) {         
+                    let finalProduct = products.find( product => product._id == cartProduct._id)           
+                    let position = products.findIndex( product => product == finalProduct )         
+                    products.splice(position,1)
+                    localStorage.setItem("products", JSON.stringify(products))
+
+                    cartProduct.disponibles++
+                }
+            })
+            localStorage.setItem("toys", JSON.stringify(cartProducts.filter(product => product.categoria == "jugueteria")))
+            localStorage.setItem("pharmacyProducts", JSON.stringify(cartProducts.filter(product => product.categoria == "farmacia")))
+
+            precioTotal = 0
+            products.forEach(product => precioTotal += product.precio)
+            createShopping(products,shopping,precioTotal,false) 
+
+        }else if(e.target.id == "eliminar"){
+            cartProducts.forEach(cartProduct => {
+                products.forEach(product => {
+                    if(product._id == cartProduct._id){
+                        cartProduct.disponibles++
+                    }
+                })
+            })
+            localStorage.setItem("toys", JSON.stringify(cartProducts.filter(product => product.categoria == "jugueteria")))
+            localStorage.setItem("pharmacyProducts", JSON.stringify(cartProducts.filter(product => product.categoria == "farmacia")))
+            products = []
+            localStorage.setItem("products", JSON.stringify(products))
+            precioTotal = 0
+            createShopping(products,shopping,precioTotal,false)
+
+        }else if(e.target.id == "comprar"){
+            products = []
+            localStorage.setItem("products", JSON.stringify(products))
+            precioTotal = 0
+            createShopping(products,shopping,precioTotal,false)
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Compra realizada con exito',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+    })
+})
+
+
+
+
 
 let slide = document.getElementById("slide")
 
--- Assets/js/module/functions.js
+++ Assets/js/module/functions.js
@@ -146,8 +146,12 @@ export function noEncontrado(container, formulario) {
   </div>`
 
 }
-
-export function createShopping(list,container,precioTotal=0){
+//
+/* <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg> */
+export function createShopping(list,container,precioTotal=0,bool){
+  let src = bool ? "../img/garbage.png" : "./Assets/img/garbage.png"
   let template = ""
   list.forEach( (element,i) => {
     template +=`
@@ -159,7 +163,9 @@ export function createShopping(list,container,precioTotal=0){
             <p>Precio: ${element.precio}</p>
           </div>
           <div class="cart-delete">
-            <button class="btn-delete"><img src="../img/garbage.png" class="garbage" alt="garbage" id="${element._id}"></button>
+            <button class="btn-delete"> <svg xmlns="http://www.w3.org/2000/svg" id="${element._id}" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <img src="${src}" class="garbage" alt="garbage" id="${element._id}"></img>
+            </button>
           </div>
         </div>
       </div>
